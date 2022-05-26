3ac9bbb8d27ea06bb03d61fcff0f6902725e0e51
Alphamplyer <even@pouyadou.fr>
2022-05-26 16:18:24 +0200

    fix: platform utils method

--- lib/src/platform.dart
+++ lib/src/platform.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 extension PlatformUtils on Platform {
-  bool get isDesktop {
+  static bool get isDesktop {
     if (kIsWeb) return false;
     return [
       TargetPlatform.linux,
